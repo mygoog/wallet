9ac39647948677365da1a7569cacf52a39c39ff8
mygoog <mygoog2000@mail.ru>
2020-10-05 12:41:28 +0500

    добавлены функции

---
+++ pkg/wallet/service_test.go
@@ -0,0 +1,99 @@
+package wallet
+
+import (
+	"fmt"
+	"testing"
+)
+
+// Автотесты для FindAccountByID
+func TestService_FindAccountByID_success(t *testing.T) {
+	svc := Service{}
+	account, err := svc.RegisterAccount("+9920000001")
+	if err != nil {
+		fmt.Println(account)
+	}
+	account, err = svc.FindAccountByID(1)
+	if err != nil {
+		t.Errorf("\ngot > %v \nwant > nil", err)
+	}
+}
+
+func TestService_FindAccountByID_notFound(t *testing.T) {
+	svc := Service{}
+	account, err := svc.RegisterAccount("+9920000001")
+	if err != nil {
+		fmt.Println(account)
+	}
+	account, err = svc.FindAccountByID(3)
+	if err == nil {
+		t.Errorf("\ngot > %v \nwant > nil", err)
+	}
+}
+
+// Автотесты для Reject
+func TestService_Reject_success(t *testing.T) {
+	svc := Service{}
+	svc.RegisterAccount("+9920000001")
+	account, err := svc.FindAccountByID(1)
+
+	if err != nil {
+		t.Errorf("\ngot > %v \nwant > nil", err)
+	}
+	err = svc.Deposit(account.ID, 1000_00)
+
+	if err != nil {
+		t.Errorf("\ngot > %v \nwant > nil", err)
+	}
+
+	payment, err := svc.Pay(account.ID, 100_00, "food")
+
+	if err != nil {
+		t.Errorf("\ngot > %v \nwant > nil", err)
+	}
+
+	pay, err := svc.FindPaymentByID(payment.ID)
+
+	if err != nil {
+		t.Errorf("\ngot > %v \nwant > nil", err)
+	}
+
+	err = svc.Reject(pay.ID)
+
+	if err != nil {
+		t.Errorf("\ngot > %v \nwant > nil", err)
+	}
+}
+
+func TestService_Reject_fail(t *testing.T) {
+	svc := Service{}
+	svc.RegisterAccount("+9920000001")
+	account, err := svc.FindAccountByID(1)
+
+	if err != nil {
+		t.Errorf("\ngot > %v \nwant > nil", err)
+	}
+	err = svc.Deposit(account.ID, 1000_00)
+
+	if err != nil {
+		t.Errorf("\ngot > %v \nwant > nil", err)
+	}
+
+	payment, err := svc.Pay(account.ID, 100_00, "food")
+
+	if err != nil {
+		t.Errorf("\ngot > %v \nwant > nil", err)
+	}
+
+	pay, err := svc.FindPaymentByID(payment.ID)
+
+	if err != nil {
+		t.Errorf("\ngot > %v \nwant > nil", err)
+	}
+
+	wrongPayID := pay.ID + "14"
+	err = svc.Reject(wrongPayID)
+
+	if err == nil {
+		t.Errorf("\ngot > %v \nwant > nil", err)
+	}
+}
