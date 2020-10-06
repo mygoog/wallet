f9a931655f17c2e76a60c7c38666fdd72adf5c87
mygoog <mygoog2000@mail.ru>
2020-10-06 11:16:31 +0500

    добавлен тип Favorite

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -33,3 +33,12 @@ type Account struct {
 	Phone Phone
 	Balance Money
 }
+
+// Favorite представляет собой Избранное
+type Favorite struct {
+	ID        string
+	AccountID int64
+	Name      string
+	Amount    Money
+	Category  PaymentCategory
+}
